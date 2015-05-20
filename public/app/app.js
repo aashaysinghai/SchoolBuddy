0e6341f377b46bd1a0221f3f550d4ff48a3a16c5
Brice Wilson <brice@5wilsons.net>
2015-05-19 22:29:20 -0400

    completed changes for m1 demo1

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,33 @@
 (function() {
 
-    var app = angular.module('app', []);
+    var app = angular.module('app', ['ngRoute']);
 
-    app.config(['$logProvider', function ($logProvider) {
+    app.config(['$logProvider', '$routeProvider', function ($logProvider, $routeProvider) {
 
         $logProvider.debugEnabled(true);
 
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                controllerAs: 'home',
+                templateUrl: 'app/templates/home.html'
+            })
+            .when('/schools', {
+                controller: 'AllSchoolsController',
+                controllerAs: 'schools',
+                templateUrl: '/app/templates/allSchools.html'
+            })
+            .when('/classrooms', {
+                controller: 'AllClassroomsController',
+                controllerAs: 'classrooms',
+                templateUrl: '/app/templates/allClassrooms.html'
+            })
+            .when('/activities', {
+                controller: 'AllActivitiesController',
+                controllerAs: 'activities',
+                templateUrl: '/app/templates/allActivities.html'
+            })
+            .otherwise('/');
     }]);
 
 }());
