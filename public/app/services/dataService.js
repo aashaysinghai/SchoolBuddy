17707e1a238c66270033170b68586a38d49a35b1
Brice Wilson <brice@5wilsons.net>
2015-05-26 23:09:49 -0400

    added delay to getActivities

--- public/app/services/dataService.js
+++ public/app/services/dataService.js
@@ -1,9 +1,9 @@
 (function () {
 
     angular.module('app')
-        .factory('dataService', ['$http', '$q', '$log', dataService]);
+        .factory('dataService', ['$http', '$q', '$log', '$timeout', dataService]);
 
-    function dataService($http, $q, $log) {
+    function dataService($http, $q, $log, $timeout) {
 
         return {
             getAllSchools: getAllSchools,
@@ -47,14 +47,24 @@
         }
 
         function getAllActivities() {
-            return $http.get('api/activities')
-                .then(function(response) {
-                    return response.data;
-                })
-                .catch(function(response) {
-                    $log.error('Error retrieving activities: ' + response.statusText);
-                    return $q.reject('Error retrieving activities.');
-                })
+
+            var deferred = $q.defer();
+
+            $timeout(function() {
+
+                $http.get('api/activities')
+                    .then(function(response) {
+                        deferred.resolve(response.data);
+                    })
+                    .catch(function(response) {
+                        $log.error('Error retrieving activities: ' + response.statusText);
+                        return $q.reject('Error retrieving activities.');
+                    });
+
+            }, 1000);
+
+            return deferred.promise;
+
         }
 
         function getMonthName(month) {
