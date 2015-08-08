50294f915173706b0966b2b14406ef93ad8e6f6a
Brice Wilson <brice@5wilsons.net>
2015-08-08 14:36:55 -0400

    recorded demo1

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,37 @@
 (function() {
 
-    var app = angular.module('app', []);
+    var app = angular.module('app', ['ui.router']);
 
-    app.config(['$logProvider', function ($logProvider) {
+    app.config(['$logProvider', '$stateProvider', function ($logProvider, $stateProvider) {
 
         $logProvider.debugEnabled(true);
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: '/app/templates/home.html',
+                controller: 'HomeController',
+                controllerAs: 'home'
+            })
+            .state('schools', {
+                url: '/schools',
+                controller: 'AllSchoolsController',
+                controllerAs: 'schools',
+                templateUrl: '/app/templates/allSchools.html'
+            })
+            .state('classrooms', {
+                url: '/classrooms',
+                controller: 'AllClassroomsController',
+                controllerAs: 'classrooms',
+                templateUrl: '/app/templates/allClassrooms.html'
+            })
+            .state('activities', {
+                url: '/activities',
+                controller: 'AllActivitiesController',
+                controllerAs: 'activities',
+                templateUrl: '/app/templates/allActivities.html'
+            });
+
 
     }]);
 
